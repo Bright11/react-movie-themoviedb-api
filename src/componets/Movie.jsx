1f479db6bdf6a4dae47cb0cb93e2c93289f819dc
Chika Nwazuo <34070274+Bright11@users.noreply.github.com>
2023-06-25 19:04:23 +0000

    Update Movie.jsx

--- src/componets/Movie.jsx
+++ src/componets/Movie.jsx
@@ -15,9 +15,8 @@ function Movie() {
 		
 		try {
 		
-        const apikey = "api_key=8cf1ea8253dc60fc82c2d83d42b81a48";
-		  const accessToken =
-        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4Y2YxZWE4MjUzZGM2MGZjODJjMmQ4M2Q0MmI4MWE0OCIsInN1YiI6IjYzMjBkZDdhYzFmZmJkMDA3ZDI0YTg2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0tFxq6j5hdN5auHQSX6YtGdotS9dZ1odWIr4fGHAO_o";
+        const apikey = "api_key=";
+		  const accessToken ="";
         const response = await axios
           .get(`${trendingurl}?${apikey}`, {
             headers: {
